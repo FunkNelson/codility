778ffcfeda55b20123ac50ab4742a94ae5e8c328
Morgan Ross <rossmb@gmail.comm>
2025-11-16 11:35:57 -0800

    create card loop

--- FlashCards.py
+++ FlashCards.py
@@ -3,14 +3,18 @@ import os
 
 cardStack = {
     'Golden Signals': 'Latency, Traffic, Error Rates, Saturation',
-    'Latency': 'Time it takes for a request to travel from the client to the server and back'
+    'Latency': 'Time it takes for a request to travel from the client to the server and back',
+    'Traffic': 'Number of requests a system receives over a specific period',
+    'Errors (Monitoring)': 'Percentage of requests resulting in errors, such as 404 Page Not Found or 500 Internal Server errors',
+    'Saturation': 'Measures resource utilization, including CPU, memory and disk space'
 }
 
 
 keys_list = list(cardStack.keys())
-random_card = random.choice(keys_list)
+
 
 terminal_width = os.get_terminal_size().columns
+session_active = True
 
 
 def displayCard(card):
@@ -36,5 +40,10 @@ def printAnswer(card):
     print('\n')
 
 
-displayCard(random_card)
-printAnswer(random_card)
+while session_active:
+    random_card = random.choice(keys_list)
+    displayCard(random_card)
+    printAnswer(random_card)
+    nextCard = input('Another card? (Y/N)'.center(terminal_width))
+    if nextCard.lower() == 'n':
+        session_active = False
